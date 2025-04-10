269211630a843f6ed30a3466de18268e799f4d7e
Beatriz Kobayashi <dev.kobayashibeatriz@gmail.com>
2025-04-10 17:35:22 -0300

    define some files in .py

--- scripts/undistort_node.py
+++ scripts/undistort_node.py
@@ -8,21 +8,29 @@ from cv_bridge import CvBridge
 class FisheyeUndistort:
     def __init__(self):
         self.bridge = CvBridge()
-        
+
         # Load calibration parameters
-        self.K = np.array(rospy.get_param("/fisheye_csi_cam/camera_matrix/data")).reshape(3,3)
-        self.D = np.array(rospy.get_param("/fisheye_csi_cam/distortion_coefficients/data"))
-        
+        cam_name = rospy.get_param("~cam_name", "csi_cam_0") # Get camera name as a private parameter
+        namespace = "/" + cam_name + "/"
+        self.K = np.array(rospy.get_param(namespace + "camera_matrix/data")).reshape(3,3)
+        self.D = np.array(rospy.get_param(namespace + "distortion_coefficients/data"))
+
+        # Get image width and height from parameters
+        self.image_width = rospy.get_param(namespace + "image_width")
+        self.image_height = rospy.get_param(namespace + "image_height")
+
         # Create Undistortion Maps (pre-compute once)
         self.map1, self.map2 = cv2.fisheye.initUndistortRectifyMap(
             self.K, self.D, np.eye(3), self.K,
-            (rospy.get_param("/fisheyecsi_cam/image_width"), rospy.get_param("/fisheye_csi_cam/image_height")),
+            (self.image_width, self.image_height),
             cv2.CV_16SC2
         )
-        
+
         # Setup subscribers/publishers
-        self.sub = rospy.Subscriber("/fisheye_csi_cam/image_raw", Image, self.image_cb)
+        self.sub = rospy.Subscriber(namespace + "image_raw", Image, self.image_cb)
         self.pub = rospy.Publisher("image_undistorted", Image, queue_size=10)
+
+        rospy.loginfo("Fisheye undistortion node started.")
 
     def image_cb(self, msg):
         try:
@@ -30,9 +38,9 @@ class FisheyeUndistort:
             undistorted = cv2.remap(cv_image, self.map1, self.map2, interpolation=cv2.INTER_LINEAR)
             self.pub.publish(self.bridge.cv2_to_imgmsg(undistorted, "bgr8"))
         except Exception as e:
-            rospy.logerr(e)
+            rospy.logerr(f"Error processing image: {e}")
 
 if __name__ == '__main__':
     rospy.init_node('fisheye_undistort_node')
-    FisheyeUndistort()
+    undistort_node = FisheyeUndistort()
     rospy.spin()
